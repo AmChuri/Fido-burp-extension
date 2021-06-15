7430a9cbb0b7abaa682d2427b5c09ee3b9a5cbe4
amayc <churi.amay@gmail.com>
2021-06-16 01:19:03 +0200

    Test time format with request header modifier

--- BurpExtender.java
+++ BurpExtender.java
@@ -2,23 +2,37 @@ package burp;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 public class BurpExtender implements IBurpExtender, IHttpListener, IProxyListener
 {
     private IExtensionHelpers helpers;
+
     PrintWriter stdout;
     @Override
 	public void registerExtenderCallbacks (IBurpExtenderCallbacks callbacks){
 // set our extension name
         callbacks.setExtensionName("Tutorial extension");
-        
         // obtain our output and error streams
         stdout = new PrintWriter(callbacks.getStdout(), true);
         PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
+        // Get current time
+        Calendar calObj = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        String time = dateFormat.format(calObj.getTime());
+        stdout.println("+---------------------------------------------------------+");
+        stdout.println("|                  BURP Extension for FIDO                |");
+        stdout.println("|                     Version 0.0.0                       |");
+        stdout.println("|                   Started @ " + time + "                    |");
+        stdout.println("+---------------------------------------------------------+");
+        
+
         
         // write a message to our output stream
         stdout.println("Hello output");
 
         helpers = callbacks.getHelpers();
+        callbacks.registerProxyListener(this);
         callbacks.registerHttpListener(this);
     }
     @Override
@@ -27,14 +41,19 @@ public class BurpExtender implements IBurpExtender, IHttpListener, IProxyListene
             IHttpService httpService = messageInfo.getHttpService();
 
             String host = httpService.getHost();
+            int port = httpService.getPort();
+
             if(host != null){
                 stdout.println(host);
+                stdout.println(port);
             }
         }
     }
 
     @Override
     public void processProxyMessage(boolean messageIsRequest, IInterceptedProxyMessage message){
+        stdout.println("Entered message request");
+        stdout.println(messageIsRequest);
         if(messageIsRequest){
             IHttpRequestResponse messageInfo = message.getMessageInfo();
             IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
